a55c9d8571623d252ccfa870bd2bf8138ff88772
jieqianzhang <zhang.jieqian@gmail.com>
2015-11-04 16:45:27 -0800

    demcands

---
+++ allcandidates_entire.js
@@ -0,0 +1,38 @@
+var allcandidates = {
+ "name": "Democratic" + "<br> "+"Debate",
+ "rank": "",
+ "size": 0,
+ "pic":  "trump.png",
+ "children": [
+  {
+   "name": "Bernie Sanders",
+   "rank": "1",
+   "size":100,
+   "pic": "sanders.png",
+   "children": [
+    {"name": "Hillary Clinton", "rank": "1", "size":100, "pic":"clinton.png"},
+    {"name": "Martin O'Malley", "rank": "2", "size":100, "pic":"omalley.png"}
+   ]
+  },
+  {
+   "name": "Hillary Clinton",
+   "rank": "2",
+   "size":100,
+   "pic": "clinton.png",
+   "children": [
+    {"name": "Bernie Sanders", "rank": "1", "size":100, "pic":"sanders.png"},
+    {"name": "Martin O'Malley","rank": "2", "size":100, "pic":"omalley.png"}
+   ]
+  },
+  {
+   "name": "Martin O'Malley",
+   "rank": "3",
+   "size":100,
+   "pic": "omalley.png",
+   "children": [
+    {"name": "Bernie Sanders", "rank": "1","size":100, "pic":"sanders.png"},
+    {"name": "Hillary Clinton", "rank": "2","size":100, "pic":"clinton.png"}
+   ]
+  }
+ ]
+};
---
+++ allcandidates_seperate.js
@@ -0,0 +1,33 @@
+var sanders = {
+   "name": "Bernie Sanders",
+   "rank": "1",
+   "size":100,
+   "pic": "sanders.png",
+   "children": [
+    {"name": "Hillary Clinton", "rank": "1", "size":100, "pic":"clinton.png"},
+    {"name": "Martin O'Malley", "rank": "2", "size":100, "pic":"omalley.png"}
+   ]
+  };
+
+var clinton = {
+   "name": "Hillary Clinton",
+   "rank": "2",
+   "size":100,
+   "pic": "clinton.png",
+   "children": [
+    {"name": "Bernie Sanders", "rank": "1", "size":100, "pic":"sanders.png"},
+    {"name": "Martin O'Malley","rank": "2", "size":100, "pic":"omalley.png"}
+   ]
+  };
+
+var omalley = {
+   "name": "Martin O'Malley",
+   "rank": "3",
+   "size":100,
+   "pic": "omalley.png",
+   "children": [
+    {"name": "Bernie Sanders", "rank": "1","size":100, "pic":"sanders.png"},
+    {"name": "Hillary Clinton", "rank": "2","size":100, "pic":"clinton.png"}
+   ]
+  };
+ 
